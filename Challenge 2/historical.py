a2c3170f3d520393c39c18ef23246b70916ad22f
Caity Bogdan <cbogdan@bu.edu>
2016-09-28 15:26:03 -0400

    Got historical view updating based on new information in data.txt file

--- Challenge 2/historical.py
+++ Challenge 2/historical.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from time import sleep
 
+# Note that in order for graphics to be externalized, it must be changed in the spyder console
+
 # Check if file exists
 find_file_timeout = 0
 timeout = 0
@@ -21,6 +23,7 @@ while True:
             timeout = 1
             break
         
+plt.figure()
 
 # Main piece of code
 if timeout == 0:
@@ -34,12 +37,45 @@ if timeout == 0:
     
         # Import data from file
         new_data = np.loadtxt("data.txt",skiprows = readrows, dtype = int)
-        readrows = readrows + len(new_data)
+
+        #find length of read rows
+        print(len(new_data.shape))
+        print(new_data.shape)
+        if len(new_data.shape) == 1:
+            readrows = readrows + 1
+            print(readrows)
+            print("I added 1")
+        else:
+            try:
+                readrows = readrows + new_data.shape[0]
+                print(readrows)
+                print("I added more than 1")
+            except:
+                pass
+                           
         
-        id.append(new_data[0,:])
-        temp.append(new_data[1,:])
-        time.append(new_data[2,:])
+        try:
+            id.extend(new_data[:,0])
+            temp.extend(new_data[:,1])
+            time.extend(new_data[:,2])        
+            new_data = []
+            plt.clf
+            plt.plot(range(len(temp)),temp, 'k')
+            plt.pause(.5)
+            print("Got here!")
     
-        new_data = []
-        plt.plot(temp)
+        except:
+            print("No New Data")
+            sleep(.1)
+        
+        # Add fake new line
+
+        target = open("data.txt",'a')
+        for n in range(2):
+            target.write("4 23 46 15 20 20 \n")
+        target.close
+            
+        
+            
     
+
