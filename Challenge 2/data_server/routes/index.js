c7b682f5cb5c637092eed85f0077c8e9fbe5e1ec
wenfeishi <wfshi@bu.edu>
2016-10-04 00:21:17 -0400

    html changes

--- Challenge 2/data_server/routes/index.js
+++ Challenge 2/data_server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile(__dirname + '/html/index.html');
+  res.sendfile(__dirname + '/html/index_html/index.html');
 });
 
 module.exports = router;
