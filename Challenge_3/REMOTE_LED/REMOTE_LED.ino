1c10af6d22be751e3a7e7dfe3e06d878f0be7dfa
Caity Bogdan <cbogdan@bu.edu>
2016-10-11 16:46:00 -0400

    Troubleshooted LED code, able to send commands via XCTU and have Arduino respond and transmit status

--- Challenge_3/REMOTE_LED/REMOTE_LED.ino
+++ Challenge_3/REMOTE_LED/REMOTE_LED.ino
@@ -25,47 +25,55 @@ void setup() {
   
   // Turn On XBEE
   XBee.begin(9600);
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  // Read in from XBee
-  reading = int(XBee.read());
-  
-  if(reading == 0){
-    digitalWrite(LED_1,0);
-    digitalWrite(LED_2,0);
-    digitalWrite(LED_3,0);
-    delay(.5);
-    }
-  else if(reading == 1){
-    digitalWrite(LED_1,1);
-    digitalWrite(LED_2,1);
-    digitalWrite(LED_3,1);
-    delay(.5);
-    }
-  else if(reading == 2){
-    for(int n = 0; n<=4; n = n+1){
-      digitalWrite(LED_1,1);
-      digitalWrite(LED_2,1);
-      digitalWrite(LED_3,1);
-      delay(.05);
+  if(XBee.available()){
+    Serial.print("Made it into loop");
+    
+    // Read in from XBee
+    state = int(XBee.read())-48;
+    
+    if(state == 0){
       digitalWrite(LED_1,0);
       digitalWrite(LED_2,0);
       digitalWrite(LED_3,0);
-      delay(.05);
+      delay(10);
       }
-    for(int n = 0; n<= 8; n = n+1){
-      digitalWrite(LED_1, random(0,1));
-      digitalWrite(LED_2, random(0,1));
-      digitalWrite(LED_3, random(0,1));
-      delay(.05);
+    else if(state == 1){
+      digitalWrite(LED_1,1);
+      digitalWrite(LED_2,1);
+      digitalWrite(LED_3,1);
+      delay(10);
       }
+    else if(state == 2){
+      for(int n = 0; n < 5; n++){
+        for(int n = 0; n <4; n++){
+          digitalWrite(LED_1,1);
+          digitalWrite(LED_2,1);
+          digitalWrite(LED_3,1);
+          delay(100);
+          digitalWrite(LED_1,0);
+          digitalWrite(LED_2,0);
+          digitalWrite(LED_3,0);
+          delay(100);
+        }
+        for(int n = 0; n < 8; n++){
+          digitalWrite(LED_1, random(0,2));
+          digitalWrite(LED_2, random(0,2));
+          digitalWrite(LED_3, random(0,2));
+          delay(100);
+          }
+      }
+      }
+
+    // Send back Status
+    Serial.println(state);
+    XBee.print(state);
+
   }
-  
-  // Send back Status
-  XBee.print(reading);
   
   }
