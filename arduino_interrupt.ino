e68ce3f4421e6c606de40c3b078a76a0719fe0f7
Aravind <aravind4113546@gmail.com>
2016-11-20 22:16:56 -0500

    Button interrupt for arduino

---
+++ arduino_interrupt.ino
@@ -0,0 +1,20 @@
+const byte interruptPin = 2;
+int flag = 0;
+
+void setup() {
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(flag)
+  {
+    Serial.println("In");
+    flag = 0;
+  }
+}
+
+void blink() {
+  flag = 1;
+}
